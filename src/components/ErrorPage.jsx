9ee893bb33e67a82636b1840d2e4de08ff34ebfc
yash-jivani <yashjivani17@gmail.com>
2023-07-06 09:02:28 +0530

    Error page

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsFileEarmarkBreak } from "react-icons/bs";
+import { links } from "./utils/helper";
+
+const ErrorPage = () => {
+    return (
+        <main className='h-screen w-full grid place-items-center'>
+            <section className='flex flex-col items-center justify-center gap-10'>
+                <header className='font-medium text-3xl flex items-center justify-center gap-1 md:text-4xl xl:text-5xl'>
+                    <h1 className='uppercase'>page not found</h1>
+                    <BsFileEarmarkBreak />
+                </header>
+                <div className='flex flex-col items-center'>
+                    <h1 className='uppercase text-clr-light-2 my-2'>
+                        quick links
+                    </h1>
+                    <ul className='gap-4 flex flex-col font-normal xl:flex xl:gap-2 xl:flex-row'>
+                        {links.map((currLink, index) => {
+                            return (
+                                <li
+                                    className='text-center text-xl uppercase xl:text-base font-normal'
+                                    key={index}
+                                >
+                                    <Link
+                                        to={currLink.path}
+                                        className='linkHover m-2'
+                                    >
+                                        {currLink.linkName}
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </section>
+        </main>
+    );
+};
+
+export default ErrorPage;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Main from "./components/Main";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
+import ErrorPage from "./components/ErrorPage";
 
 const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: "/",
